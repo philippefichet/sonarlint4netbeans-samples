0a3e775ec8eb05eb891d743b7a5f31126c0dfaf5
FICHET Philippe <philippe.fichet@laposte.net>
2022-10-30 16:17:07 +0100

    Add CND and CPPLite project

---
+++ cnd-make-project/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: FICHET Philippe &lt;philippe.fichet@laposte.net&gt;
+ *
+ * Created on 30 octobre 2022, 11:16
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("%d", 1);
+    return 0;
+}
+// CND MakeProject
+// comment not compliant with cpp:S787 */
+/* comment compliant with cpp:S787 */
+
+// C++
+class foo // Noncompliant
+{
+};
---
+++ lightweight-cpp-project-example/newCPPTemplate.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: FICHET Philippe &lt;philippe.fichet@laposte.net&gt;
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("%d", 1);
+    return 0;
+}
+// CPPLite project
+// comment not compliant with cpp:S787 */
+/* comment compliant with cpp:S787 */
+
+// C++
+class foo // Noncompliant
+{
+};
